48fafa43d2bcbd4f59f573ebb0272a327d6bed35
nilsreich <nilsreich@gmail.com>
2020-02-18 01:45:26 +0100

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,6 @@
 let count=0;
-module.exports = (res) => {
+module.exports = (res, req) => {
   count = count + 1;
+  if (req.query === 'reset){ count =0 }
   res.status(200).send(`Hello ${count}!`)
 }
